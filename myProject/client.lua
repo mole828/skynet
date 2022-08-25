259c5e91e423f7bd112df503ff6fee6de121cd90
mole828 <mrx800@163.com>
2022-08-25 17:30:25 +0800

    daily update

--- myProject/client.lua
+++ myProject/client.lua
@@ -1,13 +1,20 @@
 package.cpath = "luaclib/?.so"
 package.path = "lualib/?.lua;myProject/?.lua"
 
-local socket = require 'client.socket'
-local sproto = require 'sproto'
+local socket = require "client.socket"
+local proto = require "proto"
+local sproto = require "sproto"
+
+-- local host = sproto.new(proto.msg):host "package"
+-- local request = host:attach(sproto.new(proto.c2s))
+
+local fd = assert(socket.connect("127.0.0.1", 8888))
 
 
-local proto = require 'proto'
-
-local msg = sproto.parse(proto.msg):host "msg"
-
-local fd = assert(socket.connect('127.0.0.1', 8888))
-
+while true do
+    local cmd = socket.readstdin()
+    if cmd then
+        local package = string.pack(">s2", cmd)
+        socket.send(fd, package)
+    end
+end
--- myProject/main.lua
+++ myProject/main.lua
@@ -9,25 +9,18 @@ package.path = "lualib/?.lua;myProject/?.lua"
 local proto = require 'proto'
 
 skynet.start(function()
-    local listenfd = socket.listen("0.0.0.0", 8888)
-
-    local messager = sproto.parse(proto.msg)
+    local listenfd = socket.listen("0.0.0.0", 8888, 5)
 
     socket.start(listenfd, function(fd, addr)
         --启⽤连接
         print(fd .. " connected addr:" .. addr)
-        socket.start(fd)
         --消息处理
         while true do
             local readdata = socket.read(fd)
             --正常接收
             if readdata ~= nil then
-                
-                
                 print(fd .. " recv " .. readdata)
-                socket.write(fd, messager:encode('msg', {
-                    code = 0;
-                }))
+                socket.write(fd, readdata)
                 --断开连接
             else
                 print(fd .. " close ")
