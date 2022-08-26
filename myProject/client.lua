df98a0106ca83917cf5570b9650081e1e4a99750
mole828 <mrx800@163.com>
2022-08-26 17:03:48 +0800

    daily update

--- myProject/client.lua
+++ myProject/client.lua
@@ -5,11 +5,25 @@ local socket = require "client.socket"
 local proto = require "proto"
 local sproto = require "sproto"
 
--- local host = sproto.new(proto.msg):host "package"
--- local request = host:attach(sproto.new(proto.c2s))
+local host = sproto.new(proto.s2c):host "package"
+local request = host:attach(sproto.new(proto.c2s))
 
 local fd = assert(socket.connect("127.0.0.1", 8888))
 
+local function send_package(fd, pack)
+	local package = string.pack(">s2", pack)
+	socket.send(fd, package)
+end
+
+local session = 0
+local function send_request(name, args)
+	session = session + 1
+	local str = request(name, args, session)
+	send_package(fd, str)
+	print("Request:", session)
+end
+
+send_request("handshake")
 
 while true do
     local cmd = socket.readstdin()
