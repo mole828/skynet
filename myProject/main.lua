80a0da3beaf69a877104904b587494d71736fe41
mole828 <mrx800@163.com>
2022-09-06 16:58:34 +0800

    daily update

--- myProject/main.lua
+++ myProject/main.lua
@@ -1,41 +1,27 @@
-local skynet = require "skynet"
-local socket = require "skynet.socket"
-
 package.path = "lualib/?.lua;myProject/?.lua"
 
+local skynet = require "skynet"
 local cluster = require "skynet.cluster"
-require 'skynet.manager'
 
+local snax = require "skynet.snax"
+require "skynet.manager"
 skynet.start(function()
-    print('begin skynet.start')
-
-    local ping1 = skynet.newservice('ping')
-    local ping2 = skynet.newservice('ping')
-    skynet.send(ping1, 'lua', 'start', ping2)
-
-    print('end skynet.start')
-end)
-
-local clients = {}
-
-function connect(fd, addr)
-    print(fd.." connected addr:"..addr)
-    socket.start(fd, function (...)
-        -- nothing
-    end)
-    clients[fd]={}
-    while true do
-        local readdata = socket.read(fd)
-        if readdata~=nil then
-            print(fd.." recv "..readdata)
-            for key, value in pairs(clients) do
-                local msg = fd.." say:"..readdata
-                socket.write(key, msg, string.len(msg))
-            end
-        else
-            print(fd.." close ")
-            socket.close(fd);
-            clients[fd] = nil
-        end
+    cluster.reload({
+        node1 = "127.0.0.1:7001",
+        node2 = "127.0.0.1:7002",
+    })
+    skynet.error "cluster init"
+    local mynode = skynet.getenv("node")
+    skynet.error (mynode)
+    if mynode == "node1" then
+        cluster.open("node1")
+        local ping1 = skynet.newservice("ping")
+        local ping2 = skynet.newservice("ping")
+        skynet.send(ping1, "lua", "start", "node2", "pong")
+        skynet.send(ping2, "lua", "start", "node2", "pong")
+    elseif mynode == "node2" then
+        cluster.open("node2")
+        local ping3 = skynet.newservice("ping")
+        skynet.name("pong", ping3)
     end
-end
+end)
--- myProject/ping.lua
+++ myProject/ping.lua
@@ -1,11 +1,25 @@
 local skynet = require "skynet"
-local CMD = {
-    start = function (source)
-        print(source)
-    end
-}
+local cluster = require "skynet.cluster"
+local mynode = skynet.getenv("node")
+
+local CMD = {}
+function CMD.ping(source, source_node, source_srv, count)
+    local id = skynet.self()
+    skynet.error("[" .. id .. "] recv ping count=" .. count)
+    skynet.sleep(100)
+    cluster.send(source_node, source_srv, "ping", mynode,
+        skynet.self(), count + 1)
+end
+
+function CMD.start(source, target_node, target)
+    cluster.send(target_node, target, "ping", mynode,
+        skynet.self(), 1)
+end
+
 skynet.start(function()
     skynet.dispatch("lua", function(session, source, cmd, ...)
+        skynet.error(debug.getinfo(1).short_src .. ":" .. debug.getinfo(1).currentline,
+            "(" .. session .. "," .. source .. "," .. cmd .. ")", ...)
         local f = assert(CMD[cmd])
         f(source, ...)
     end)
